1a899139245c8c80c60d85d0580dec5e943575dd
Mohamed ELHIRECH <mohamed.elhirech@prestataire.sacd.fr>
2023-12-26 16:51:13 +0100

    change design and fix overlay size

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,8 @@ const PEXELS_API_KEY = 'OSCvPMha37JH3k7KcmPqG2OOozAP0iiQmQDG2rno0sC54ZrFwaBrS7v2
 const PEXELS_API_URL = "https://api.pexels.com/v1/search?query=nature&per_page=100"
 
 const imagesPath = path.join(__dirname, '../public/images/')
-const overlayLightPath = imagesPath + 'textOverlayLight.png'
-const overlayDarkPath = imagesPath + 'textOverlayDark.png'
+const overlayLightPath = imagesPath + 'overlay.svg'
+const overlayDarkPath = imagesPath + 'overlayDark.svg'
 
 var fs = require('fs');
 var outputDir = imagesPath + 'output'
@@ -23,22 +23,25 @@ if (!fs.existsSync(outputDir)){
 
 const processImage = (imageInput, folder) => new Promise(async (resolve, reject) => {
   const { src, height, width, center = true } = imageInput
-
+  
     const isURL = src.includes(('http'))
     const uri = isURL ? src : src.split(';base64,').pop()
     const imgBuffer = isURL ? (await axios({ url: uri, responseType: "arraybuffer" })).data : Buffer.from(uri, 'base64');
 
+    try {
+      
     const metadata = await sharp(imgBuffer).metadata();
     const { dominant } = await sharp(imgBuffer).stats();
     const { r, g, b } = dominant;
     const color = tinycolor({ r, g, b });
 
     const BOTTOM_LEFT = { left: 10, top: metadata.height - 40  }
-    const overlayText = color.isLight() ? overlayDarkPath : overlayLightPath 
+    const overlayText = color.isLight() ? overlayDarkPath : overlayLightPath
+    // const rotatedOverlay = await sharp(overlayText).rotate(-45, { background: 'transparent' }).toBuffer() 
 
     const leftLayer = { input: overlayText, ...BOTTOM_LEFT }
     const centerLayer = { input: overlayText, gravity: 'centre' }
-
+    
     const compositeImage = sharp(imgBuffer)
     .composite([ center ? centerLayer : leftLayer])
 
@@ -48,6 +51,9 @@ const processImage = (imageInput, folder) => new Promise(async (resolve, reject)
   
     const imageToBase64 = `data:image/png;base64,${processedImage.toString('base64')}`
     resolve({ src: imageToBase64, height, width })
+    } catch (error) {
+      resolve({ src, height, width })
+    }
 });
 
 const archiveImages = () => {
